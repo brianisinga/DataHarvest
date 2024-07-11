3f23ac43c137189fa3ac9fb1f5ca175ec45d6f49
Brian Isinga <brianisinga38@gmail.com>
2024-07-11 23:01:05 +0300

    added more raw

--- weather.py
+++ weather.py
@@ -9,8 +9,16 @@ forecast=bs(r.content, 'lxml')
 #wthr = forecast.findAll('div', {'class':'b-forecast__table-days-content'})
 #for i in range(7):
  #   print(wthr[i].text)
-
+#humidity
 whtr1 = forecast.find('tr',{'class': 'b-forecast__table-humidity js-humidity'})
 mywhtr2=whtr1.findAll('span',{'class':'b-forecast__table-value'})
 
-print(mywhtr2[0].text)
+for i in range(1,36):
+    print(mywhtr2[i].text)
+
+#high temperature
+whtr2 = forecast.find("tr",{'class': 'b-forecast__table-max-temperature js-temp'})
+mywhtr3 = whtr2.findAll('span',{'class': 'temp b-forecast__table-value'})
+
+for i in range(1, 36):
+    print(mywhtr3[i].text)
